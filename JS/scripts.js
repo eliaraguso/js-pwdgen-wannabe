c699b5330eeb0c5497fbca8cd7fd2e1408d596c9
eliaraguso <elia.raguso@gmail.com>
2021-07-28 14:05:03 +0200

    creato generatore di password

---
+++ JS/scripts.js
@@ -0,0 +1,17 @@
+// Chiedi all' utente il suo nome
+var nomeUtente = prompt("Inserisci il tuo nome");
+
+// Chiedi all' utente il suo cognome
+var cognomeUtente = prompt("Inserisci il tuo cognome");
+
+// Chiedi all' utente il suo colore preferito
+var colorePreferito = prompt("Inserisci il tuo colore preferito");
+
+// Chiedi all'utente il numero della sua classe
+var numeroClasse = parseInt(prompt("Inserisci il numero della tua classe"))
+
+// Genera la password con struttura "nomecognomecolorepreferito40"
+var password = nomeUtente + cognomeUtente + colorePreferito + numeroClasse
+
+// Scrivi sulla pagina la password
+document.getElementById('JS').innerHTML += password
